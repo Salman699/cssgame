602d1ea1d0d83d894e8b1aa2e953f7252d9e44a3
Salman699 <salmantubagus43@gmail.com>
2023-11-01 08:04:48 +0700

    css game

---
+++ js/messages.js
@@ -0,0 +1,110 @@
+var messages = {
+  languageActive: {
+    'en': 'English',
+    
+    'id': 'Bahasa Indonesia',
+    
+  },
+  next: {
+    'en': 'Next',
+    
+    'id': 'Lanjut',
+    
+  },
+  labelFooter: {
+    'en': 'Flexbox Froggy is created by',
+    
+    'id': 'Flexbox Froggy dibuat oleh',
+    
+  },
+  labelLevel: {
+    'en': 'Level',
+    
+    'id': 'Tingkat',
+    
+  },
+  labelOf: {
+    'en': 'of',
+    
+    'id': 'dari',
+    
+  },
+  title: {
+    'en': 'Flexbox Froggy - A game for learning CSS flexbox',
+    
+    'id': 'Flexbox Froggy - Sebuah permainan untuk belajar CSS flexbox',
+    
+  },
+  labelReset: {
+    'en': 'Reset',
+    
+    'id': 'Atur Ulang',
+    
+  },
+  warningReset: {
+    'en': 'Are you sure you want to reset the game?\n\nYour saved progress will be lost and you\'ll be sent to the start of the game.',
+    
+    'id': 'Apakah Anda yakin ingin mengatur ulang game?\n\n Proses Anda yang tersimpan akan hilang dan Anda akan dikirim ke awal game.',
+    
+  },
+  gridGarden: {
+    'en': 'Want to learn CSS grid? Play',
+    
+    'id': 'Ingin mempelajari CSS grid? Main',
+    
+  },
+  labelLanguage: {
+    'en': 'Language',
+    
+    'id': 'Bahasa',
+    
+  },
+  labelSettings: {
+    'en': 'Settings',
+    
+    'id': 'Pengaturan',
+    
+  },
+  labelDifficulty: {
+    'en': 'Difficulty',
+    
+    'id': 'Tingkat Kesulitan',
+    
+  },
+  labelColorblind: {
+    'en': 'Colorblind Mode',
+    
+    'id': 'Mode Buta Warna',
+    
+  },
+  labelColorblindOff: {
+    'en': 'Off',
+    
+    'id': 'Matikan',
+    
+  },
+  labelColorblindOn: {
+    'en': 'On',
+    
+    'id': 'Aktifkan',
+
+  },
+  labelDifficultyEasy: {
+    'en': 'Beginner',
+    
+    'id': 'pemula',
+    
+  },
+  labelDifficultyMedium: {
+    'en': 'Intermediate - No Directions',
+    
+    'id': 'Medium- tidak ada arahan',
+    
+  },
+  labelDifficultyHard: {
+    'en': 'Expert - No Directions & Random Levels',
+    
+    'id': 'Ahli - Tidak ada arahan & Tingkat permainan Acak',
+    
+  }
+};
